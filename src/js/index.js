7ad2f8e891d61d425e522dc0aaa1787dd0550171
Isadora Riegert <isadora.riegert@gmail.com>
2021-04-15 21:13:34 -0300

    feat(menu): upload new menu - responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-var misterious = document.querySelector(".misterious");
+// var misterious = document.querySelector(".misterious");
 
-misterious.addEventListener("click", function() {
-    document.querySelector(".container").classList.toggle("show-menu");
-});
+// misterious.addEventListener("click", function() {
+//     document.querySelector(".container").classList.toggle("show-menu");
+// });
 
 /* Um jeito de otimizar o código seria passar do estado de uma função direto para outra função.
 Como a segunda função não possui nome, pode-se transformar em um menu function com =>
