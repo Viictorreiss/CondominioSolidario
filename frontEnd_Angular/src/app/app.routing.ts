1307f061df75db81ed011b012adde6226725488f
Victor Mendes Reis <61296590+Viictorreiss@users.noreply.github.com>
2021-06-02 13:58:45 -0300

    wip
    wip

--- frontEnd_Angular/src/app/app.routing.ts
+++ frontEnd_Angular/src/app/app.routing.ts
@@ -4,27 +4,26 @@ import { CadastroCondominioComponent } from './cadastro-condominio/cadastro-cond
 import { CadastroUserComponent } from './cadastro-user/cadastro-user.component';
 import { LoginUserComponent } from './login-user/login-user.component';
 import { HomeComponent } from './home/home.component';
-import { MuralComponent } from './mural/mural.component';
-
-
+import { PerfilSindComponent } from './perfil-sind/perfil-sind.component';
+import { PerfilUsuComponent } from './perfil-usu/perfil-usu.component';
 
 const appRoutes: Routes = [
-   { 
-      path: '', 
-      component: HomeComponent 
+   {
+      path: '',
+      component: HomeComponent
    },
    {
-      path: 'mural',
-      component: MuralComponent
-   },
-   { 
-      path: 'cadastro', 
-      component: CadastroUserComponent 
+      path: 'cadastro',
+      component: CadastroUserComponent
    },
    {
       path: 'login',
       component: LoginUserComponent
    },
+   {
+    path: 'perfil',
+    component: PerfilSindComponent
+ },
 
    // otherwise redirect to home
    // { path: '**', redirectTo: '' }
